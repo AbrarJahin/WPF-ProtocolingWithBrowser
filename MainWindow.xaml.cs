0bfa08e281617335d98135475883ad872667a3c5
S. M. Abrar Jahin <AbrarJahin@users.noreply.github.com>
2019-10-01 11:03:54 +0600

    Invoke a get method

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -32,14 +33,38 @@ namespace Protocoling
                 //on the arguments passed in
                 string[] arguments = Environment.GetCommandLineArgs();
                 MessageBox.Show("CMD Param: \n\n" + arguments[1]);
+
+                //######################################################################################//
+                //Documentation can be found in here- http://restsharp.org/
+                //And here- https://stackoverflow.com/questions/6999598/how-to-perform-a-get-request-with-restsharp
+
+                RestClient client = new RestClient("http://dummy.restapiexample.com/api/v1/employees");
+                var request = new RestRequest(Method.GET);
+
+                client.ExecuteAsync(request, response => {
+                    // do something with the response
+                    var a = response;
+
+                    if (response.ResponseStatus == ResponseStatus.Error)
+                    {
+                        //failure(response.ErrorMessage);
+                        MessageBox.Show("Failed to connect");
+                    }
+                    else
+                    {
+                        MessageBox.Show(response.Content);
+
+                        //If done, then exit the client
+                        Environment.Exit(0);
+                    }
+                });
+                //######################################################################################//
             }
             else
             {
                 //Open the WPF UI
                 MessageBox.Show("Clicked");
             }
-            //Exit program if no error has occured
-            System.Windows.Application.Current.Shutdown();
         }
     }
 }
